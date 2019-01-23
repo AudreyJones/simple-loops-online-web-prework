b502480d65029bf59e6cae45b4ee44bc79d2d1dd
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-23 15:41:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,10 +35,8 @@ def until_iterator(number_of_times)
 end
 ################################################################################
 #For Loop
-number_of_times = (1..7)
-
 def for_iterator(number_of_times)
-  for items in number_of_times
+  for count in 1..number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
